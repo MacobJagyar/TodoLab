a3550eb51dfdc9cb11649cb953f1026353e1e47c
MacobJagyar <jacobmagyar95@gmail.com>
2022-10-31 22:45:00 -0400

    finished to-do lab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
+import { compileClassMetadata } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { takeLast } from 'rxjs';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todolab';
+
+  todoArr: Todo[] = [
+    {task: "Take out trash",
+    completed: true},
+    {task: "Do dishes",
+    completed: false},
+    {task: "Sweep floors",
+    completed: false},
+    {task: "Clean windows",
+    completed: true}
+  ]
+
+  addTodo(formParam: NgForm): void {
+    let newTodo: Todo = {
+      task: formParam.form.value.todo,
+      completed: false
+    };
+    this.todoArr.push(newTodo);
 }
+
+completeTask(task: Todo): void {
+  task.completed = true;
+}
+
+undoTask(task: Todo): void {
+  task.completed = false;
+}
+
+removeTask(i: number): void {
+ let text = "Are you sure you want to remove this task? Press OK to confirm or Cancel to go back without making changes."
+ if (confirm(text) === true){
+  this.todoArr.splice(i, 1);
+ }
+}
+
+}
+
+
+
+
+
+
